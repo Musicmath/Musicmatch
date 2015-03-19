2771ef81d789567661ce73c236ec02b90bccf1df
Gustav Sundström <gustav.sundstrom.te12@svall.ntig.se>
2015-03-19 11:09:56 +0100

    new stuff
    stuff is new

---
+++ musicmaatch/Js/popoutform.js
@@ -0,0 +1,26 @@
+﻿
+// Validating Empty Field
+function check_empty() {
+if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
+alert("Fill All Fields !");
+} else {
+document.getElementById('form').submit();
+alert("Form Submitted Successfully...");
+}
+}
+//Function To Display Popup
+function div_show() {
+document.getElementById('create').style.display = "block";
+}
+//Function to Hide Popup
+function div_hide(){
+document.getElementById('create').style.display = "none";
+}
+
+function div_show1() {
+document.getElementById('login').style.display = "block";
+}
+//Function to Hide Popup
+function div_hide1(){
+document.getElementById('login').style.display = "none";
+}
---
+++ musicmaatch/Js/scripts.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function(){
+	var show = 0;
+	$('.menu-btn').click(function()){
+	
+	$('.nav > li' ).css({display:'block'}); /*Gör så att när man klickar på knappen som tar fram menyn på mobilversionen att den droppas ner */
+	
+	});
+
+});
